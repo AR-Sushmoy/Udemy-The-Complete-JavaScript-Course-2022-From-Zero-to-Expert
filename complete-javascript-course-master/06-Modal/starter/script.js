89976bf235e5461ce8262f6d5c182490232ea539
Sushmoy~ <sushmoy9166@gmail.com>
2023-03-09 10:24:51 +0600

    finished module 07 notes in this update

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -2,11 +2,12 @@
 
 //////////////////////////////////////////
 ///////// PROJECT #2_ Modal Window
-
+/*
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnShowModal = document.querySelectorAll('.show-modal');
+*/
 // If we use this .querySelectorAll() it selects all the elements associate with the class name show-modal and it basically creates a list or array of these elements.
 /*
 console.log(btnShowModal); // Output: NodeList(3) [button.show-modal, button.show-modal, button.show-modal]
@@ -35,7 +36,7 @@ for (let i = 0; i < btnShowModal.length; i++) {
 
     // Now let's remove the hidden class.
 
-    // So, what we want to do is take the modal. So this element (document.querySelector('.modal');) that we selected here previously. So we selected it, then stored it into this variable and named it modal At Line 06. And now we take that selected element and on there we can read the class list property And this class list property itself has a couple of methods.
+    // So, what we want to do is take the modal. So this element (document.querySelector('.modal');) that we selected here previously. So we selected it, then stored it inside this variable modal At Line 06. And now we take that selected element and on there we can read the class list property And this class list property itself has a couple of methods.
 
     // And the one we are gonna use now is remove() . The class that we are gonna remove is the class called "hidden" .
 
@@ -64,7 +65,7 @@ overlay.addEventListener('click', function () {
 */
 
 // However, we do not want the same code here in btnCloseModal and overlay. We want actually the exact same function to be executed in both of these places, no matter if we click the on the close button or if we click on the overlay. So, how should we do that? We can simply export this function into a more real named function.
-
+/*
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
@@ -73,8 +74,9 @@ const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+*/
 // so now, this variable here closeModal is what holds the function value.
-
+/*
 for (let i = 0; i < btnShowModal.length; i++) {
   btnShowModal[i].addEventListener('click', openModal);
 }
@@ -82,9 +84,147 @@ for (let i = 0; i < btnShowModal.length; i++) {
 btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
-
+*/
 // notice that we are not calling the function. We are not doing this --> closeModal() .This would not work at all, because it will immediately called a function, as soon as JavaScript executes this line.
 
 //But that's not what we want to happen. We want the closeModal function to be executed only as soon as the click event happens on the closeModal button.
 
 // In Line: 81 & 83, That's so much better than what we had before. Not only did we make our code more DRY. But we also made it more readable. More expressive, and more declarative. So right now, when we read this code here, it's a lot easier to understand.
+
+//////////////////////////////////////////
+///////// Handling an _Esc_ Keypress Event
+
+// How to responde to Keyboard events? such as, when we press the ESC key on our keyboard the modal window will close.
+
+// So, in order to listen for keyboard events we still need to use add event listener.
+
+// Because the keypress event is simply just another type of event.
+
+// Now, Keyboard events are so called global events because they do not happen on one specific event. And, for global events like keyboard events we usually listen on the whole document.
+
+// By using addEventListener here on the document we are listening for events everywhere. So, no mater where they happen on the page they will always trigger the event handler that we are gonna specify here.
+
+// Now, about the name of the event itself there are three types of event for the keyboard. i) keydown, ii) keypress and iii) keyup
+
+// keyup only happens when we lift our finger off the key
+
+// keypress is fired continuously as we keep our finger on a certain key
+
+// keydown is fired as soon as we press down the key. And usually this is the one that we use.
+
+/*
+document.addEventListener('keydown', function () {
+  // this function() here will be executed for any key press that happens.
+  console.log('A key was pressed'); //Output: A key was pressed
+});
+*/
+
+// However we only want to close the pop-up window when the ESC key was pressed.
+
+// How will we know which key was pressed if the event here happens for all the keys?
+
+// Well, the information about which key was pressed will be stored in the event that is going to occure as soon as any key is pressed.
+
+// So remember as we hit any key on our keyboard a keydown event is generated and our handler function is waiting for that to happen.
+
+// And any time an event like this occures javaScript does in fact generates an object and that object contains all the information about the event itself and we can then actually access the object in the event handler function.
+
+// Now up until this point we never looked at the event. We only just listend for it and then reacted to it.
+
+// But this time we actually need to look at the event object in order to figure out which key was pressed.
+
+// how can we do that? well, what we need to do is to give this function() here a parameter. let's call it e which stands for event. But we could give it any name it doesn't matter.
+
+/*
+document.addEventListener('keydown', function (e) {
+  //
+});
+*/
+
+// So, then as the event occures javaScript will call this function with the event object as an argument. And this happens because we do not call this function by ourselves.
+
+// We do not call the function, We only define it here. So we say hey, javaScript call this function() when a keydown event happens and when you do so please pass in the object as an argument.
+
+/*
+document.addEventListener('keydown', function (e) {
+  console.log(e); // if we press the enter key. output: KeyboardEvent {isTrusted: true, key: "Enter", code: "Enter" and so on.....}
+  // It's just an object that is created by JavaScript.
+});
+*/
+
+// So, that's great because when we press any key now we can see which key we just pressed.
+
+// if we look closely inside the object We can see that we get the information from the key property in the object.
+
+// So, let's log the key property in the console
+
+/*
+document.addEventListener('keydown', function (e) {
+  console.log(e.key); // now if we press the escape key we get output: Escape
+});
+*/
+
+// Great, now that we know which key was actually pressed we can use that information to close the modal window whenever the escape key is pressed.
+
+// We only want to close the modal when the modal is actually visible. And, How do we know if the modal is currently visible?
+
+// well, if the modal contains class hidden it means that it's not visible and when it does not contain the class hidden it means it's visible then that's the condition in which we want to close it.
+
+// Now, from the previous lecture [Working With Classes] we learned about classList property and its methods remove() and add().
+
+// We can also check if an element contains a class.
+/*
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    // Condition explanation: if the key we pressed is Escape and if modal does not contains the class hidden then execute rest of the code.
+    closeModal(); // this function has the code to remove the hidden both for the modal window and the overlay.
+  }
+});
+*/
+
+///// Final Program:
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', function () {
+    // modal.classList.remove('hidden');
+    // overlay.classList.remove('hidden');
+    openModalAndOverlay();
+  });
+}
+
+btnCloseModal.addEventListener('click', function () {
+  // modal.classList.add('hidden');
+  // overlay.classList.add('hidden');
+  closeModalAndOverlay();
+});
+
+overlay.addEventListener('click', function () {
+  // modal.classList.add('hidden');
+  // overlay.classList.add('hidden');
+  closeModalAndOverlay();
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    // modal.classList.add('hidden');
+    // overlay.classList.add('hidden');
+    closeModalAndOverlay();
+  }
+});
+
+// above here in our code we did not followed the DRY principle. So, Let's implement that into our program.
+
+function closeModalAndOverlay() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+function openModalAndOverlay() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,1 +1,310 @@
 'use strict';
+/////////////////////////////////////////
+//////// 015 PROJECT #3_ Pig Game
+
+/*
+// Selecting elements
+const score0El = document.getElementById('score--0'); // this way we can catch a ID.
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// Starting Conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+*/
+
+/*
+console.log(typeof score0El.textContent); // output: string
+*/
+
+/////////////////////////////////////////
+///////// 016 Rolling the Dice
+
+/*
+// Selecting elements
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+let currentScore = 0; // we started at zero and it needs to be a let beacuse we will keep updating it.
+
+// this variable currentScore can not be inside of the eventListener function because then it would be set to zero each time that we clicked the button.
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  //   console.log(dice);
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1
+  if (dice !== 1) {
+    // Add dice to the current score
+    currentScore += dice;
+    // console.log(currentScore);
+    current0El.textContent = currentScore; // CHANGE LATER, because in the future, we will need to display this current score at the current player and not always add player number one or player number two. but for now we just want this functionality work.
+  } else {
+    // Switch to next player
+  }
+});
+*/
+
+////////////////////////////////////////////
+//////// 017 Switching the Active Player
+
+// In this lecture we're gonna be concerned with switching from one active player to another one.
+
+/*
+// Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+let activePlayer = 0; // Since we start with the first player we set it to 0 because remember player1 is player--0 and player2 is player--1.
+
+// Explanation: The reason is that we will store the final scores of both players in an array
+
+const scores = [0, 0]; // index 0 is for the final score for player1 and index 1 is for the final score for player2.
+
+// Rolling dice functoinality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display Dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1
+  if (dice !== 1) {
+    // Add dice to the current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore; // So, the ID name is gonna be either current--0 for player1 or current--1 for player2. we've used the variable activePlayer basically to build this ID name.
+
+    // This is a very handy trick of building the ID name dynamically.
+  }
+  // Whenever a player rolls a one we'll enter the else block. This means when a dice is not a one then the dice gets added to the currentScore variable but if we happen to roll a 1, then we lose all our score and it is the next players round.
+
+  // In the previous lecture, we implemented this functionality of adding the dice value to the currentScore variable but only as the player1.
+
+  // But now we need to make this work for both player1 and player2.
+
+  // And in order to do that, we actually need to keep track of which player is actually the active player in the moment that the dice button was clicked. Basically we need to know which player is right now playing. By right now means whenever the button is clicked.
+
+  // So, We will create another variable which will hold exactly that. So, it will hold 0, if the current player is player--0 and it will hold 1 if the current player is player--1. At line 60.
+  else {
+    // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0; // This way we'll be able to change the activePlayer value from 0 to 1 or from 1 to 0.
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+    // What toggle() will do is that, it will add the class if it is not there and if it is there, it will remove it.
+
+    // Since we start with the player active class only on one element then toggling both at the same time will ensure that it's only ever on one of the elements at once.
+  }
+});
+*/
+////////////////////////////////////////////
+//////// 018 Holding Current Score
+
+// At line 68: To store the current score in the final score we are using this scores array.
+
+// sroces[0] means player1 and scores[1] means player2.
+
+// Now, we can actually use this activePlayer variable to store the current score of the current players final score.
+
+/*
+let playing = true;
+
+function switchPlayer() {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+}
+// Above created this function sperately because we are using this same program twice in two buttons. Usually, when we are refactoring something than there is many times like a small thing that changes in the code. Then it's very useful to have a parameter so that when we call the function, we can specify what changes. But here nothing changes all we want to do is to simply repeat this code.
+
+// And so we don't need any parameters and we don't need to return anything, This is simply just a reusable piece of code.
+
+// Rolling dice functoinality
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    // Basically, all of this logic inside that we have here should only be executed if we are playing. Playing itself is already a boolean.
+
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // 2. Display Dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // 3. Check for rolled 1
+    if (dice !== 1) {
+      // Add dice to the current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  // 1. Add current score to active player's final score
+
+  if (playing) {
+    scores[activePlayer] += currentScore; // This stores the current score to the scores Array at index depending on the activePlayer variable.
+    // let's say current player is player1 which means activePlayer variable is at 0. So, it workes like this, scores[0] = scores[0] + currentScore;
+
+    // Now let's display it
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. check is player's score is greater or equal than 100
+    // Finish the game
+    if (scores[activePlayer] >= 30) {
+      playing = false; // Okay, now this in itself will,  of course not yet do anything. So we only defined whether the game is playing or not, but none of our code is reacting to that. So remember that we only want to be able to click on these buttons once we are playing. And so let's define that At Line: 136 with a condition.
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // 3. Switch to the next player
+      switchPlayer();
+    }
+  }
+  // At this stage the first problem is that it still showes the dice image, we have to get rid of that.
+
+  // But, Even worse is that we can still keep playing the game after a player wins the game. That we need to solve.
+
+  // The solution is to create a variable that hosts the state of the game. Basically means if we are still playing or not.
+
+  // So, this is gonna be a state variable which kinds of tells us the condition of a system. In this case the condition will be if the game playing or not?
+
+  // And, so if the game is playing then we can click these buttons and then everything will work as normally. But then as soon as the game is finished we will say that the game is no longer playing and we can no longer click on these buttons.
+
+  // Let's declare a state variable named playing and set it to ture At Line: 120 and then when we finish the game we'll set playing to false.
+});
+*/
+
+////////////////////////////////////////////
+////////  019 Resetting the Game
+
+// Final Program for this project: (Without any notes)
+
+// Selecting elements
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let currentScore, activePlayer, playing, scores;
+
+const init = function () {
+  // Starting Conditions
+  diceEl.classList.add('hidden');
+
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  scores = [0, 0];
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+};
+
+init();
+
+function switchPlayer() {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+}
+
+// Rolling dice functoinality
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // 2. Display Dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // 3. Check for rolled 1
+    if (dice !== 1) {
+      // Add dice to the current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  // 1. Add current score to active player's final score
+
+  if (playing) {
+    scores[activePlayer] += currentScore;
+
+    // Now let's display it
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. check is player's score is greater or equal than 100
+    // Finish the game
+    if (scores[activePlayer] >= 30) {
+      playing = false;
+
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // 3. Switch to the next player
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', init);
+
+// Basically, we refactor our code in this lecture to the fullest.
+
+// N.B: Re-watch the video for better explanation for this lecture, notes won't do it justice. 😁
